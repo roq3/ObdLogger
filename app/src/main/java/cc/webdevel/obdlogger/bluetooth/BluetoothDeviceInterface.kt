ed3ccf9fdb7e1be108ad33e00375931b8e6930de
roque <gitlab@roque.pl>
2024-12-27 14:50:09 +0100

    new create Record

--- app/src/main/java/cc/webdevel/obdlogger/bluetooth/BluetoothDeviceInterface.kt
+++ app/src/main/java/cc/webdevel/obdlogger/bluetooth/BluetoothDeviceInterface.kt
@@ -6,4 +6,5 @@ interface BluetoothDeviceInterface {
     fun getName(): String
     fun getAddress(): String
     fun createRfcommSocketToServiceRecord(uuid: UUID?): BluetoothSocketInterface
+    fun createInsecureRfcommSocketToServiceRecord(uuid: UUID?): BluetoothSocketInterface
 }
